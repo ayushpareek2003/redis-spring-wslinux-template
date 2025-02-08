1c290eb758643f720a53d95fefe9025af3d3f302
Ayush Pareek <AYUSHPAREEK1980@GMAIL.COM>
2025-02-08 21:40:21 +0530

    Add files via upload

---
+++ config_r/RedisConfig.java
@@ -0,0 +1,26 @@
+package com. "add you package path " .config_r; // change this according to you 
+
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+        @Bean
+        public RedisTemplate redisTemplate(RedisConnectionFactory connectionFactory) {
+            RedisTemplate redisTemplate = new RedisTemplate();
+            redisTemplate.setConnectionFactory(connectionFactory);
+
+            redisTemplate.setKeySerializer(new StringRedisSerializer());
+
+            redisTemplate.setValueSerializer(new StringRedisSerializer());
+
+            return redisTemplate;
+
+        }
+}

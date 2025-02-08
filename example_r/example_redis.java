a8487f35798d72ee90e0c4ddfb79df4c414fbe42
Ayush Pareek <AYUSHPAREEK1980@GMAIL.COM>
2025-02-08 21:48:54 +0530

    how to use

--- example_r/example_redis.java
+++ example_r/example_redis.java
@@ -7,6 +7,8 @@ import net. "add your package path" .constants.Placeholders;
 
 
 
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
